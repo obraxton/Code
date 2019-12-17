c323589b72d5b68675385c3d9a51bfd601eca20a
Otis Braxton <obraxton@mail.com>
2019-12-17 00:49:40 -0700

    arguments in fibonacci

--- prime.cpp
+++ prime.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <cstdlib>
 #include <math.h>
 
 using namespace std;
-int main() {
-    unsigned long long int i=0, j=0;
+int main(int argc, char *argv[]) {
+    
+    unsigned long long int i=1, j=0, max=0;
+    max = strtol(argv[1], nullptr, 0);
     bool prime = true;
-    while ( i < __UINT64_MAX__ ) {
+    while ( i < max ) {
         i++;
         j = 2;
         prime = true;
