d8d9c4eec3c84f1a13508835a037b3af14e2ee29
Otis Braxton <obraxton@mail.com>
2019-12-17 23:21:50 -0700

    Full arg handling with start \& end

--- prime.cpp
+++ prime.cpp
@@ -5,23 +5,46 @@
 using namespace std;
 int main(int argc, char *argv[]) {
     
-    unsigned long long int i=1, j=0, max=0;
-    max = strtol(argv[1], nullptr, 0);
+    unsigned long long int start=1, j=0, end=0;
+    switch (argc) {
+    case 2:
+        start = 1;
+        if (argv[1] == "max") {
+            end = __UINT64_MAX__;
+        } else {
+            end = strtol(argv[1], nullptr, 0);
+        }
+        break;
+
+    case 3:
+        start = strtol(argv[1], nullptr, 0);
+        if (argv[2] == "max") {
+            end = __UINT64_MAX__;
+        } else {
+            end = strtol(argv[2], nullptr, 0);
+        }
+        break;
+    default:
+        cout << "Usage: prime [start] end" << endl;
+        cout << endl << "end can be 'max'" << endl << endl;
+        return -1;
+        break;
+    }
+    if (start >= end) { return -1; }
+    
     bool prime = true;
-    while ( i < max ) {
-        i++;
+    while ( start < end ) {
+        start++;
         j = 2;
         prime = true;
-        while (j < sqrt(i))
-        {
-            if (i % j == 0)
-            {
+        while (j < sqrt(start)) {
+            if (start % j == 0) {
                 prime = false;
                 break;
             }
             j++;
         }
-        if (prime == true) {cout << i << endl;}        
+        if (prime == true) { cout << start << endl; }        
     }
 
     return 0;
